696de562f77bd92be760bbe68e3ac25ca8f0e74b
Manon GABARRET <manon.gabarret@teamwork.net>
2022-12-08 02:10:37 +0100

    clean part1 day7

--- src/2022/day7.py
+++ src/2022/day7.py
@@ -1,107 +1,139 @@
-filesystem = {}
-
-
-def parse_input(puzzle, filesystem):
-    # idx_command = [command[0] for command in enumerate(puzzle) if not command[1].startswith("$")]
-    # lili = [idx_command[0]]
-    # for i in range(len(idx_command)-1):
-    #     if idx_command[i] + 1 == idx_command[i+1]:
-    #         continue
-    #     else:
-    #         lili.append(idx_command[i+1])
-    # print(lili)
-    # for command in puzzle:
-    #     if command.startswith("$ cd "):
-    #         split_command = command.split("$ cd ")
-    #         if split_command[1] != "..":
-    #             filesystem[split_command[1]] =
-
-    current_path = None
-    new = {}
-    for i, command in enumerate(puzzle):
-        if command.startswith("$ cd "):
-            split_command = command.split("$ cd ")
-            current_path = split_command[1]
-            new[split_command[1]] = []
-            # start at i+2 because i is cd line, and i +1 is ls line
-            for commandf in puzzle[i + 2 :]:
-                if not commandf.startswith("$"):
-                    new[split_command[1]].append(commandf)
+def part1(puzzle):
+    directories = {}
+    current_path = "/"
+    for command in puzzle:
+        if command[0] == "$":
+            split_cmd = command.split(" ")
+            if split_cmd[1] == "cd":
+                if split_cmd[2] != "..":
+                    path = split_cmd[2]
+                    if current_path != "/":
+                        current_path = current_path + "/" + path
+                    else:
+                        current_path = path
+                    directories[current_path] = []
                 else:
-                    break
-
-    print(new)
-    folder = {i: new[i] for i in new if i != "/"}
-    res = test(new["/"], folder)
-    print(res)
-    result = transform_int(res)
-    print(result)
-    summ = calcul_total(result, [])
-    res = sum(get_minus(summ))
-    return res
-
-
-def test(base, new):
-
-    for i, el in enumerate(base):
-        print(el)
-        if el.startswith("dir"):
-            direc = el.split(" ")
-            base.insert(i, test(new[direc[1]], new))
-            base.remove(el)
-    return base
-
-
-def transform_int(lili):
-    for i, el in enumerate(lili):
-        if isinstance(el, str):
-            red = el.split(" ")
-            lili.insert(i, int(red[0]))
-            lili.remove(el)
-        elif isinstance(el, list):
-            transform_int(el)
-    return lili
-
-
-def nested_sum(L):
-    total = 0
-    for i in L:
-        if isinstance(i, list):
-            total += nested_sum(i)
+                    list_currant_path = current_path.split("/")
+                    remove_last = list_currant_path[:-1]
+                    current_path = "/".join(remove_last)
         else:
-            total += i
-    return total
+            split_cmd = command.split(" ")
+            if split_cmd[0] != "dir":
+                directories[current_path].append(split_cmd[0])
+    for keys in directories.keys():
+        if not keys.startswith("/"):
+            directories[f"/{keys}"] = directories.pop(keys)
+    return directories
 
 
-def kjfbvkej(tot):
-    list_inf = []
-    total = nested_sum(tot)
-    if total > 10000:
-        for el in tot:
-            if isinstance(el, list):
-                total = kjfbvkej(el)
-    else:
-        list_inf.append(total)
-        print(total)
-    return list_inf
+def create_size_folder(directories):
+    size_dir = {}
+    for keys, values in directories.items():
+        if "/" in keys[1:]:
+            all_dir = keys[1:].split("/")
 
 
-def calcul_total(result, list_inf):
-    for el in result:
-        print(el)
-        if isinstance(el, list):
-            print(nested_sum(el))
-            list_inf.append(nested_sum(el))
-            calcul_total(el, list_inf)
-        # else:
-        #     list_inf.append(el)
-    return list_inf
+# def parse_input(puzzle, filesystem):
+#     # idx_command = [command[0] for command in enumerate(puzzle) if not command[1].startswith("$")]
+#     # lili = [idx_command[0]]
+#     # for i in range(len(idx_command)-1):
+#     #     if idx_command[i] + 1 == idx_command[i+1]:
+#     #         continue
+#     #     else:
+#     #         lili.append(idx_command[i+1])
+#     # print(lili)
+#     # for command in puzzle:
+#     #     if command.startswith("$ cd "):
+#     #         split_command = command.split("$ cd ")
+#     #         if split_command[1] != "..":
+#     #             filesystem[split_command[1]] =
+#
+#     current_path = None
+#     new = {}
+#     for i, command in enumerate(puzzle):
+#         if command.startswith("$ cd "):
+#             split_command = command.split("$ cd ")
+#             current_path = split_command[1]
+#             new[split_command[1]] = []
+#             # start at i+2 because i is cd line, and i +1 is ls line
+#             for commandf in puzzle[i + 2 :]:
+#                 if not commandf.startswith("$"):
+#                     new[split_command[1]].append(commandf)
+#                 else:
+#                     break
+#
+#     print(new)
+#     folder = {i: new[i] for i in new if i != "/"}
+#     res = test(new["/"], folder)
+#     print(res)
+#     result = transform_int(res)
+#     print(result)
+#     summ = calcul_total(result, [])
+#     res = sum(get_minus(summ))
+#     return res
 
 
-def get_minus(list_inf):
-    final_list = [el for el in list_inf if el < 100000]
-    print(final_list)
-    return final_list
+# def test(base, new):
+#
+#     for i, el in enumerate(base):
+#         print(el)
+#         if el.startswith("dir"):
+#             direc = el.split(" ")
+#             base.insert(i, test(new[direc[1]], new))
+#             base.remove(el)
+#     return base
+#
+#
+# def transform_int(lili):
+#     for i, el in enumerate(lili):
+#         if isinstance(el, str):
+#             red = el.split(" ")
+#             lili.insert(i, int(red[0]))
+#             lili.remove(el)
+#         elif isinstance(el, list):
+#             transform_int(el)
+#     return lili
+
+
+# def nested_sum(L):
+#     total = 0
+#     for i in L:
+#         if isinstance(i, list):
+#             total += nested_sum(i)
+#         else:
+#             total += i
+#     return total
+#
+#
+# def kjfbvkej(tot):
+#     list_inf = []
+#     total = nested_sum(tot)
+#     if total > 10000:
+#         for el in tot:
+#             if isinstance(el, list):
+#                 total = kjfbvkej(el)
+#     else:
+#         list_inf.append(total)
+#         print(total)
+#     return list_inf
+
+
+# def calcul_total(result, list_inf):
+#     for el in result:
+#         print(el)
+#         if isinstance(el, list):
+#             print(nested_sum(el))
+#             list_inf.append(nested_sum(el))
+#             calcul_total(el, list_inf)
+#         # else:
+#         #     list_inf.append(el)
+#     return list_inf
+#
+#
+# def get_minus(list_inf):
+#     final_list = [el for el in list_inf if el < 100000]
+#     print(final_list)
+#     return final_list
 
 
 # def recusive(base, new):
@@ -134,7 +166,7 @@ def get_minus(list_inf):
 
 
 if __name__ == "__main__":
-    with open("2022/inputs/input7", "r") as file:
-        # with open("2022/inputs/test_input", "r") as file:
-        result = parse_input(file.read().splitlines(), filesystem)
+    # with open("2022/inputs/input7", "r") as file:
+    with open("2022/inputs/test_input", "r") as file:
+        result = part1(file.read().splitlines())
     print(f"The result is: {result}")

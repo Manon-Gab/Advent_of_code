75e08687c70dc26c957fb11b622b7687a5f1a49f
Manon GABARRET <manon.gabarret@teamwork.net>
2022-12-10 23:23:04 +0100

    day10 part2

--- src/2022/day10.py
+++ src/2022/day10.py
@@ -18,6 +18,33 @@ def part1(puzzle):
     return sum(signal_strengths)
 
 
+def part2(puzzle):
+    program = []
+    for cycle in range(40):
+        sprite_position = [0, 1, 2]
+        pixel = 0
+
+
+def split_puzzle_multiple_40(puzzle):
+    cycle = 0
+    puzzle_by_cycle = []
+    multiples_40 = find_multiples_40(146)
+    for instruction in puzzle:
+        circuit = []
+        if instruction == "noop":
+            circuit.append(instruction)
+            cycle += 1
+            if cycle in multiples_40:
+                puzzle_by_cycle.append(circuit)
+        else:
+            circuit.append(instruction)
+            for _ in range(2):
+                cycle += 1
+                if cycle in multiples_40:
+                    puzzle_by_cycle.append(circuit)
+    return puzzle_by_cycle
+
+
 def find_total_cycles(puzzle):
     len_instruction_two_cycle = len([el for el in puzzle if el != "noop"])
     len_instruction_one_cycle = len([el for el in puzzle if el == "noop"])
@@ -29,8 +56,12 @@ def find_multiples(total_cycle):
     return [i for i in range(20, total_cycle, 40)]
 
 
+def find_multiples_40(total_cycle):
+    return [i for i in range(0, total_cycle, 40)]
+
+
 if __name__ == "__main__":
     with open("2022/inputs/input10", "r") as file:
         # with open("2022/inputs/test_input", "r") as file:
-        result = part1(file.read().splitlines())
+        result = split_puzzle_multiple_40(file.read().splitlines())
     print(f"The result is: {result}")

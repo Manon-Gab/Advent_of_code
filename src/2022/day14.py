4615c6bb08315c8e4a94f4bc4bf00fbe619c3740
Manon GABARRET <manon.gabarret@teamwork.net>
2022-12-15 22:52:03 +0100

    day14 part 1 reKO

--- src/2022/day14.py
+++ src/2022/day14.py
@@ -1,10 +1,54 @@
 def part1(puzzle):
     rock_paths = parse_puzzle(puzzle)
-    min_y, max_y, min_x, max_x = find_min_max(rock_paths)
-    empty_scan = [["." for _ in range(max_y + 1)] for _ in range(max_x + 1)]
-    # add base part
-    scan = drawing_rock(empty_scan, rock_paths)
+    max_y, max_x = find_max(rock_paths)
+    empty_scan = [["." for _ in range(max_y + 2)] for _ in range(max_x + 1)]
+
     # complete rock path
+    new_rock_paths = complete_rock_path(rock_paths)
+    # drawing the scan
+    scan = drawing_rock(empty_scan, new_rock_paths)
+    scan[0][500] = "+"
+    for row in scan:
+        print(row[479:])
+
+    # add the units of sand in the scan
+    try:
+        for j in range(1000):
+            for x in range(len(scan) - 1):
+                add_sand(scan, x, 500)
+    except IndexError:
+        scan[x][500] = "A"
+        print(f" x is : {x}")
+        print(f" j is : {j}")
+    counter = count_units_of_sand(scan)
+    # for row in scan:
+    #     print(row[478:])
+    return counter
+
+
+def parse_puzzle(puzzle):
+    rock_paths = [
+        [[int(el) for el in coord.split(",")] for coord in line]
+        for line in [rock_path.split(" -> ") for rock_path in puzzle]
+    ]
+    return rock_paths
+
+
+def find_max(rock_paths):
+    max_y = max([coord[0] for row in rock_paths for coord in row])
+    max_x = max([coord[1] for row in rock_paths for coord in row])
+    return int(max_y), int(max_x)
+
+
+def drawing_rock(scan, rock_paths):
+    for path in rock_paths:
+        for idx, coord in enumerate(path):
+            scan[coord[1]][coord[0]] = "#"
+    return scan
+
+
+def complete_rock_path(rock_paths):
+    """Complete the list of rock coord."""
     new_rock_paths = []
     for path in rock_paths:
         add_to_path = path
@@ -13,40 +57,16 @@ def part1(puzzle):
             if coord_to_add:
                 add_to_path.extend(coord_to_add)
         new_rock_paths.append(add_to_path)
-
-    scan = drawing_rock(scan, new_rock_paths)
-    try:
-        for _ in range(10000000):
-            for x in range(len(scan) - 1):
-                add_sand(scan, x, 500)
-    except IndexError:
-        pass
-    counter = 0
-    for row in scan:
-        for unit in row:
-            if unit == "o":
-                counter += 1
-    # for row in scan:
-    #     print(row[490:515])
-    return counter
+    return new_rock_paths
 
 
 def add_sand(scan, x, y):
     if scan[x + 1][y] == ".":
         return add_sand(scan, x + 1, y)
 
-    # Left edge management
-    elif y == 0 and scan[x + 1][y] != "." and scan[x + 1][y + 1] != ".":
-        scan[x][y] = "o"
-        return True
-
     elif scan[x + 1][y] != "." and scan[x + 1][y - 1] == ".":
         return add_sand(scan, x + 1, y - 1)
 
-    # Right edge management
-    elif y == len(scan[0]) and scan[x + 1][y - 1] != "." and scan[x + 1][y] != ".":
-        scan[x][y] = "o"
-        return True
     elif (
         scan[x + 1][y] != "."
         and scan[x + 1][y - 1] != "."
@@ -59,30 +79,8 @@ def add_sand(scan, x, y):
         and scan[x + 1][y + 1] != "."
     ):
         scan[x][y] = "o"
+        print(y)
         return True
-
-
-def drawing_rock(scan, rock_paths):
-    for path in rock_paths:
-        for idx, coord in enumerate(path):
-            scan[coord[1]][coord[0]] = "#"
-    return scan
-
-
-def parse_puzzle(puzzle):
-    rock_paths = [
-        [[int(el) for el in coord.split(",")] for coord in line]
-        for line in [rock_path.split(" -> ") for rock_path in puzzle]
-    ]
-    return rock_paths
-
-
-def find_min_max(rock_paths):
-    min_y = min([coord[0] for row in rock_paths for coord in row])
-    max_y = max([coord[0] for row in rock_paths for coord in row])
-    min_x = min([coord[1] for row in rock_paths for coord in row])
-    max_x = max([coord[1] for row in rock_paths for coord in row])
-    return int(min_y), int(max_y), int(min_x), int(max_x)
 
 
 def compare_two_coord(coord_1, coord_2):
@@ -103,6 +101,15 @@ def compare_two_coord(coord_1, coord_2):
             for i in range(coord_2[0] + 1, coord_1[0]):
                 to_add.append([i, coord_1[1]])
     return to_add
+
+
+def count_units_of_sand(scan):
+    counter = 0
+    for row in scan:
+        for unit in row:
+            if unit == "o":
+                counter += 1
+    return counter
 
 
 if __name__ == "__main__":
